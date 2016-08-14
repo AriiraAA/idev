7be5ae4500cca0a2c58e6939ecc04046ace9b08f
Azhary Arliansyah <09021181419007@students.ilkom.unsri.ac.id>
2016-08-14 20:45:29 +0700

    Update connect.php

--- aset/connect.php
+++ aset/connect.php
@@ -1,8 +1,8 @@
 <?php 
 	$host = "localhost";
-	$user = "root";
-	$pass = "";
-	$db   = "idev";
+	$user = "b10d2143076fa9";
+	$pass = "83353f7f";
+	$db   = "ilecturer_unsri";
 	$link = mysql_connect($host, $user, $pass) or die ('Koneksi Gagal!');
 	mysql_select_db($db); 
 ?>
